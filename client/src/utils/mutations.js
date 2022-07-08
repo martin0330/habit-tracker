1e7fc24362bb1cadcb9a797f74c3691ba2daf00c
martin0330 <martin.cruz0330@yahoo.com>
2022-07-07 20:02:36 -0400

    added mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,52 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_HABIT = gql`
+    mutation saveHabit($input: habitInput!) {
+        saveHabit(input: $input) {
+            _id
+            username
+            email
+            savedHabits {
+                habitName
+                completedAt
+            }
+        }
+    }
+`;
+
+export const Remove_habit = gql`
+    mutation removeHabit($habitId: String!) {
+        removeHabit(habitId: $habitId) {
+            _id
+            username
+            savedHabits {
+                habitName
+                completedAt
+            }
+        }
+    }
+`;
