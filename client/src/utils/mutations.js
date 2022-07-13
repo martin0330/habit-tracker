381d3ecd4e5e125c673bc4a9abbc673ff30658e1
mannygarcia98 <mannysg88@gmail.com>
2022-07-12 19:53:52 -0500

    add 'addHabit' to front end mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,52 +1,61 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const SAVE_HABIT = gql`
-    mutation saveHabit($input: habitInput!) {
-        saveHabit(input: $input) {
-            _id
-            username
-            email
-            savedHabits {
-                habitName
-                completedAt
-            }
-        }
+  mutation saveHabit($input: habitInput!) {
+    saveHabit(input: $input) {
+      _id
+      username
+      email
+      savedHabits {
+        habitName
+        completedAt
+      }
     }
+  }
+`;
+
+export const ADD_HABIT = gql`
+  mutation addHabit($habitName: String!) {
+    addHabit(habitName: $habitName) {
+      _id
+      habitName
+    }
+  }
 `;
 
 export const REMOVE_habit = gql`
-    mutation removeHabit($habitId: String!) {
-        removeHabit(habitId: $habitId) {
-            _id
-            username
-            savedHabits {
-                habitName
-                completedAt
-            }
-        }
+  mutation removeHabit($habitId: String!) {
+    removeHabit(habitId: $habitId) {
+      _id
+      username
+      savedHabits {
+        habitName
+        completedAt
+      }
     }
+  }
 `;
